4be076482601a22fb9bef15afcc7ffc2ffb86ae6
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-08-23 11:20:44 +0000

    [Finish]4-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_051332) do
+ActiveRecord::Schema.define(version: 2022_08_23_111000) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,11 @@ ActiveRecord::Schema.define(version: 2022_08_23_051332) do
   create_table "imgs", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
