1d0b330e75653ef4bfc4a6129c1c3595134c48e4
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-08-24 12:54:37 +0000

    4-11[Start]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,32 +2,5 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  def new
-    @post_image= PostImage.new
-  end
-
-  def create
-    @post_image = PostImage.new(post_image_params)
-    @post_image.urer_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
-  end
-
-  def index
-  end
-
-  def show
-
-  end
-
-  def destroy
-  end
-
-
-  private
-
-  def post_image_params
-    params.require(:post_image).permit(:shop_name,:image,:caption)
-  end
 
 end
--- db/migrate/20220822085433_create_imgs.rb
+++
@@ -1,9 +0,0 @@
-class CreateImgs < ActiveRecord::Migration[6.1]
-  def change
-    create_table :imgs do |t|
-      t.string "title"
-      t.string "body"
-      t.timestamps
-    end
-  end
-end
