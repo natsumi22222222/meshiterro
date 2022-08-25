605f62338ac958a11fbf8df64b54501ee0fca6d6
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-08-25 13:16:15 +0000

    [Finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root "homes#top"
   devise_for :users
 
-  resources :users,only:[:show,:edit]
+  resources :users,only:[:show,:edit,:update]
   resources :post_images,only:[:new,:create,:index,:show,:destroy]
   get "homes/about" => "homes#about", as: "about"
 end
